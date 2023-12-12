97dfbf51670d57399503ab12cfde03a8371f465b
CamilHamami <152988491+CamilHamami@users.noreply.github.com>
2023-12-12 14:42:41 +0100

    Update Chatgpt.py

--- Chatgpt.py
+++ Chatgpt.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"  # Replace with your actual OpenAI API key
+openai.api_key = "mykey"  # Replace with your actual OpenAI API key
 
 response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
