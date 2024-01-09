d38727c8859dca131a3fed6f821babca25040f77
CamilHamami <152988491+CamilHamami@users.noreply.github.com>
2024-01-09 14:17:17 +0100

    Update Chatgpt.py

--- Chatgpt.py
+++ Chatgpt.py
@@ -3,7 +3,8 @@ from openai import OpenAI
 import os
 
 # Replace "your_actual_api_key" with your OpenAI API key
-os.environ["OPENAI_API_KEY"] = "mykey"
+os.environ["OPENAI_API_KEY"] = "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"
+
 def ask_gpt(commande, file):
   # Ouvrir le fichier en mode lecture ('r' pour read)
   with open(file, 'r') as fichier:
@@ -12,17 +13,39 @@ def ask_gpt(commande, file):
 
   client = OpenAI()
 
-  Prompt = "this is a python code :\n" + contenu + "\n" + commande + "Format your response by:\n1. first showing the block to replace to replace\n2. Showing the whole modified code"
+  Prompt = "this is a python code :\n" + contenu + "\n" + commande + "Format your response by:\n1. first showing the block to replace to replace\n2. Showing the modified block of code"
 
 
   response = client.chat.completions.create(
-    model="gpt-4",
+    model="gpt-4-1106-preview",
     messages=[
       {"role": "system", "content": "You are a helpful assistant."},
       {"role": "user", "content": Prompt}
     ]
   )
 
-  print(response.choices[0].message.content)
+  answer = response.choices[0].message.content
+  return answer
 
-ask_gpt("Change the name of the second function please and modify the symbols",'openFile.py')
+def extract_command(gptCommand):
+      
+  blocks = []
+  temp = ""
+  writing = False
+
+  for line in gptCommand.splitlines():
+    if line == "```" : 
+      writing = False
+      blocks.append(temp)
+      temp = ""
+
+    if writing :
+      temp += line
+      temp+="\n"
+
+    if line == "```python" : 
+      writing = True
+
+  return blocks
+
+#print(ask_gpt("Change les symboles du jeu du morpion",'openFile.py'))
